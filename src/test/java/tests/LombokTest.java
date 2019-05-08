f1df0b644c878d8cadfc76b2fe660be9a6fb9389
Martin Kellogg <kelloggm@cs.washington.edu>
2019-05-08 15:31:01 -0700

    add logic so that lombok is available during testCompile, and the test suite actually uses it

---
+++ src/test/java/tests/LombokTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import org.checkerframework.framework.test.PerDirectorySuite;
+import org.checkerframework.framework.test.TestConfiguration;
+import org.checkerframework.framework.test.TestUtilities;
+import org.checkerframework.framework.test.TypecheckExecutor;
+import org.checkerframework.framework.test.TypecheckResult;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.checkerframework.framework.test.TestConfigurationBuilder.buildDefaultConfiguration;
+
+/**
+ * Custom test runner that uses the Checker Framework's tooling.
+ *
+ * The standard CF tooling isn't used because another annotation processor
+ * needs to be used.
+ */
+@RunWith(PerDirectorySuite.class)
+public class LombokTest {
+
+    private final String testDir = "lombok";
+
+    private final String[] checkerOptions = {"-Anomsgtext", "-nowarn"};
+
+    private final List<File> testFiles;
+
+    private final String[] processors = {"lombok.launch.AnnotationProcessorHider$AnnotationProcessor",
+            "org.checkerframework.checker.builder.TypesafeBuilderChecker"};
+
+    public LombokTest(List<File> testFiles) {
+        this.testFiles = testFiles;
+    }
+
+    @Test
+    public void run() {
+        boolean shouldEmitDebugInfo = TestUtilities.getShouldEmitDebugInfo();
+        List<String> customizedOptions = Arrays.asList(checkerOptions);
+        List<String> processorNames = Arrays.asList(processors);
+        TestConfiguration config =
+                buildDefaultConfiguration(
+                        testDir,
+                        testFiles,
+                        processorNames,
+                        customizedOptions,
+                        shouldEmitDebugInfo);
+        TypecheckResult testResult = new TypecheckExecutor().runTest(config);
+        TestUtilities.assertResultsAreValid(testResult);
+    }
+
+    @Parameters
+    public static String[] getTestDirs() {
+        return new String[] {"lombok"};
+    }
+}
